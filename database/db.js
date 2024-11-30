303d20c67691144a28d7c890fa2b2ceb9c0fa5a9
filipeventuri <filipeventuri@icloud.com>
2024-11-30 15:23:28 -0300

    update

--- database/db.js
+++ database/db.js
@@ -1,9 +1,15 @@
 const sequelize = require("sequelize");
 
-const connection = new sequelize ('delta', 'root', '123456789', {
-    host: 'localhost',
-    dialect: 'mysql',
-    timezone: '-03:00'
+const connection = new sequelize ('grupo0', 'grupo0_vszf_user', 'z34EigsHXMsAe0YXihNAzSIqjQ4mnsbu', {
+    host: 'dpg-ct5knejqf0us7387jc5g-a',
+    port: '5432',
+    dialect: 'postgres',
+    dialectOptions: {
+    ssl: {
+      require: true, // Requer SSL para o Render
+      rejectUnauthorized: false, // Aceita certificados não verificados
+    },
+  },
 })
 
 module.exports = connection;
